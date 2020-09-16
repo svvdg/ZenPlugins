fd9e07467e150b0ccdb790911266336ac7d7bff8
svvdg <66954710+svvdg@users.noreply.github.com>
2020-09-16 09:32:06 +0300

    [skbbank] v2_index

--- src/plugins/skbbank/index.js
+++ src/plugins/skbbank/index.js
@@ -5,10 +5,14 @@ export async function scrape ({ preferences, fromDate, toDate }) {
   await login(preferences.login, preferences.password)
   const products = await fetchProducts()
   let accounts = products.cards.map(convertCard).filter(x => !!x)
+  let accountIds = new Set(findId(convertCard))
   accounts = accounts.concat(products.accounts.map(convertAccount).filter(x => !!x))
+  accountIds = Set.add(findId(convertAccount))
   accounts = accounts.concat(products.deposits.map(convertDeposit).filter(x => !!x))
+  accountIds = Set.add(findId(convertDeposit))
   accounts = accounts.concat(products.loans.map(convertLoan).filter(x => !!x))
-  const accountIds = Object.fromEntries(accounts.map(findId)) // или accounts.set(findId) ???
+  accountIds = Set.add(findId(convertLoan))
+  // const accountIds = Object.fromEntries(accounts.map(findId)) // или const accountIds = new Set(findId(accounts)) ???
   const operations = await fetchTransactions(fromDate, toDate)
   const transactions = operations.map(transaction => convertTransaction(transaction, accounts, accountIds))
   return {
